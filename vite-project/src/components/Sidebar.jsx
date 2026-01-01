3070b30d61984bc936a27a509164780d73bb046a
Stephen Daniel Kurah <bakomamman@gmail.com>
2026-01-01 15:51:20 +0100

    Make sidebar profile clickable like Facebook

--- vite-project/src/components/Sidebar.jsx
+++ vite-project/src/components/Sidebar.jsx
@@ -1,22 +1,31 @@
-import { Link } from "react-router-dom";
 import "./Sidebar.css";
+import { Link } from "react-router-dom";
 
 function Sidebar() {
   const user = JSON.parse(localStorage.getItem("user"));
 
   return (
     <div className="sidebar">
-      <Link to="/" style={{ textDecoration: "none", color: "inherit" }}>
-        <p>🏠 Home</p>
+      {/* Profile Card */}
+      <Link to={`/profile/${user.username}`} className="profile-link">
+        <div className="sidebar-profile">
+          <img
+            src={`https://pyrexxbook-backend.onrender.com${user.avatar}`}
+            alt=""
+          />
+          <span>{user.name}</span>
+        </div>
       </Link>
 
-      <Link to={`profile/${user.username}`} style={{ textDecoration: "none", color: "inherit" }}>
-        <p>👤 Profile</p>
-      </Link>
-
-      <p>👥 Friends</p>
-      <p>🖼 Photos</p>
-      <p>⚙ Settings</p>
+      <div className="sidebar-menu">
+        <p>🤖 Meta AI</p>
+        <p>👥 Friends</p>
+        <p>📊 Professional dashboard</p>
+        <p>🕒 Memories</p>
+        <p>💾 Saved</p>
+        <p>👪 Groups</p>
+        <p>➕ See more</p>
+      </div>
     </div>
   );
 }
