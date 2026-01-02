cdb541edc43fe0d9863252efe3769149711e73ac
Stephen Daniel Kurah <bakomamman@gmail.com>
2026-01-02 03:35:47 +0100

    Force frontend to MongoDB backend

--- vite-project/src/utils/api.js
+++ vite-project/src/utils/api.js
@@ -1,6 +1,2 @@
-const API_BASE =
-  window.location.hostname === "localhost"
-    ? "http://localhost:5000"
-    : "https://pyrexxbook-kurah-backend.onrender.com";
-
+const API_BASE = "https://pyrexxbook-kurah-backend.onrender.com";
 export default API_BASE;
