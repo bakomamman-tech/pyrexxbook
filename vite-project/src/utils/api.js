ad4eb17d22f3e72b928096b12cb9b2a5be6f66ab
Stephen Daniel Kurah <bakomamman@gmail.com>
2026-01-02 03:28:23 +0100

    Connect frontend to MongoDB backend

--- vite-project/src/utils/api.js
+++ vite-project/src/utils/api.js
@@ -1,6 +1,6 @@
 const API_BASE =
   window.location.hostname === "localhost"
     ? "http://localhost:5000"
-    : "https://pyrexxbook-kurahz.onrender.com";
+    : "https://pyrexxbook-kurah-backend.onrender.com";
 
 export default API_BASE;
